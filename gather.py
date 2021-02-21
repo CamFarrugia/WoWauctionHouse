58336016e31213c5d626c38444ac40d40b7fbbb4
Drew Bonasera <Drewsif@users.noreply.github.com>
2021-02-21 05:27:22 -0500

    Lotto changes

--- gather.py
+++ gather.py
@@ -2,7 +2,11 @@ import requests
 import datetime
 import json
 import gzip
+import time
+import xmltodict
+import shelve
 import os
+import argparse
 
 
 def create_access_token(client_id, client_secret):
@@ -20,22 +24,65 @@ def get_region_ah_data(token, server):
 	data = response.json()
 	id = data['results'][0]['data']['id']
 
-	response = requests.get('https://us.api.blizzard.com/data/wow/connected-realm/60/auctions?namespace=dynamic-us&locale=en_US&access_token=%s' % token)
+	response = requests.get('https://us.api.blizzard.com/data/wow/connected-realm/%s/auctions?namespace=dynamic-us&locale=en_US&access_token=%s' % (id, token))
 	if response.status_code != 200:
 		raise Exception('Failed to get auction house data')
 	data = response.json()
 	return data['auctions']
 
 
+def enrich_data(data, outputdir):
+	t = time.time()
+	with shelve.open(os.path.join(outputdir, 'itemdb')) as item_data:
+		for auction in data:
+			item_id = str(auction['item']['id'])
+			# If we dont have the iteam data in cache download it
+			if item_id not in item_data:
+				response = requests.get('https://www.wowhead.com/item=%s&xml' % item_id)
+				if response.status_code != 200:
+					print('Failed to get item', item_id, 'data')
+					continue
+				idata = xmltodict.parse(response.text)['wowhead']['item']
+				del idata['htmlTooltip']
+				del idata['link']
+				del idata['@id']
+				idata['json'] = json.loads("{%s}" % idata['json'])
+				if 'jsonEquip' in idata:
+					idata['jsonEquip'] = json.loads("{%s}" % idata['jsonEquip'])
+				item_data[item_id] = idata
+
+			auction['ts'] = t
+			#auction['item_name'] = item_data[item_id]['name']
+			#if '#text' in item_data[item_id]['class']:
+			#	auction['item_type'] = item_data[item_id]['class']['#text']
+			#else:
+			#	auction['item_type'] = item_data[item_id]['class']['@id']
+	return data
+
+
+def _parse_args():
+	parser = argparse.ArgumentParser(description='Download WoW Auction House data')
+	parser.add_argument('--server', '-s', type=str, nargs='+', default=['Stormrage'])
+	parser.add_argument('--config', '-c', type=str, default=os.path.join(os.path.dirname(__file__), 'config.json'))
+	parser.add_argument('--output-dir', '-o', type=str, default=os.path.join(os.path.dirname(__file__), 'data'))
+	args = parser.parse_args()
+	if not os.path.isdir(args.output_dir):
+		os.mkdir(args.output_dir)
+	return args
+
+
 def main():
-	with open(os.path.join(os.path.dirname(__file__), 'config.json'), 'r') as f:
+	args = _parse_args()
+	with open(args.config, 'r') as f:
 		conf = json.loads(f.read())
 
-	token = create_access_token(conf['client_id'], conf['client_secret'])
-	data = get_region_ah_data(token, 'Stormrage')
+	for server in args.server:
+		token = create_access_token(conf['client_id'], conf['client_secret'])
+		data = get_region_ah_data(token, server)
+		data = enrich_data(data, args.output_dir)
 
-	with gzip.open(datetime.datetime.now().strftime('Stormrage_US-%Y-%m-%d-%H-%M.json.gz'), 'wb', 9) as f:
-		f.write(json.dumps(data).encode('utf8'))
+		with gzip.open(os.path.join(args.output_dir, datetime.datetime.now().strftime(server + '_US-%Y-%m-%d-%H-%M.json.gz')), 'wb', 9) as f:
+			f.write(json.dumps(data).encode('utf8'))
 
 
 if __name__ == '__main__':
---
+++ json2csv.py
@@ -0,0 +1,59 @@
+import argparse
+import json
+import csv
+import gzip
+import shelve
+
+
+def convert(jsonfile, itemdb):
+	if jsonfile.endswith('json.gz'):
+		newname = jsonfile[:-7] + 'csv'
+		fo = gzip.open
+	elif jsonfile.endswith('json'):
+		newname = jsonfile[:-4] + 'csv'
+		fo = open
+	else:
+		raise Exception("Unknown file extension on %s" % jsonfile)
+
+	if itemdb.endswith(".bak") or itemdb.endswith(".dat") or itemdb.endswith(".dir"):
+		itemdb = itemdb[:-4]
+
+	with fo(jsonfile, 'r') as f:
+		data = json.loads(f.read())
+
+	fields = ['id', 'item_name', 'item_type', 'time_left', 'quantity', 'buyout', 'unit_price']
+	with shelve.open(itemdb) as item_data:
+		for auction in data:
+			item_id = str(auction['item']['id'])
+			del auction['item']['id']
+			auction['item_id'] = item_id
+			auction['item_name'] = item_data[item_id]['name']
+			if '#text' in item_data[item_id]['class']:
+				auction['item_type'] = item_data[item_id]['class']['#text']
+			else:
+				auction['item_type'] = item_data[item_id]['class']['@id']
+			for k in auction:
+				if k not in fields:
+					fields.append(k)
+
+	with open(newname, 'w', newline='') as f:
+		writer = csv.DictWriter(f, fields)
+		writer.writeheader()
+		writer.writerows(data)
+
+
+def _parse_args():
+	parser = argparse.ArgumentParser(description='Convert Auction House JSON into a CSV')
+	parser.add_argument('itemdb')
+	parser.add_argument('jsonfile', nargs='+')
+	return parser.parse_args()
+
+
+def main():
+	args = _parse_args()
+	for f in args.jsonfile:
+		convert(f, args.itemdb)
+
+
+if __name__ == '__main__':
+	main()
